4738454e1e9653ff1b7ed4f79f50180b130ead7c
Griffin <griffincriste@gmail.com>
2019-09-09 13:49:35 -0500

    addedd more css

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,6 +6,7 @@ const Main = props => (
     <div className="pics" onClick={() => props.shuffleCards(props.id)}>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
+        <p className = "picname"> {props.name}</p>
       </div>
     </div>
   );
